b9154bb22b8edc5f65758efcb2ae38899947ee15
Youssef Tawfik <youssef.k.tawfik@gmail.com>
2024-07-26 09:02:15 +0300

    starting navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        primary: "0 0px 5px rgba(193, 61, 61, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
